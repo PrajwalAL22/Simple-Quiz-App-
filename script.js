de8951a4fecb033fa0fbf19e0c2313b89b62e199
PrajwalAL22 <144423812+PrajwalAL22@users.noreply.github.com>
2023-09-11 23:53:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,156 @@
+const questions = [
+	{
+		question: "CSS stands for -",
+		answers: [
+			{ text: "Cascade style sheets", correct: false },
+			{ text: "Color and style sheets", correct: false },
+			{ text: "Cascading style sheets", correct: true },
+			{ text: "None of the above", correct: false },
+		]
+	},
+
+	{
+		question: " Are the negative values allowed in padding property?",
+		answers: [
+			{ text: "yes", correct: false },
+			{ text: "No", correct: true },
+			{ text: "Can't Say", correct: false },
+			{ text: "May be", correct: false },
+		]
+	},
+
+	{
+		question: "The property in CSS used to change the text color of an element is -",
+		answers: [
+			{ text: "bgcolor", correct: false },
+			{ text: "color", correct: true },
+			{ text: "background-color", correct: false },
+			{ text: "None of the above", correct: false },
+		]
+	},
+
+	{
+
+		question: "The property in CSS used to change the background color of an element is -",
+		answers: [
+			{ text: "bgcolor", correct: false },
+			{ text: "Color", correct: false },
+			{ text: "background-color", correct: true },
+			{ text: "All of the above", correct: false },
+		]
+	},
+
+	{
+
+		question: "Which of the following CSS property is used to set the background image of an element?",
+		answers: [
+			{ text: "background-attachment", correct: false },
+			{ text: "background-image", correct: true },
+			{ text: "background-color", correct: false },
+			{ text: "None of the above", correct: false },
+		]
+	}
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+	currentQuestionIndex = 0;
+	score = 0;
+	nextButton.innerHTML = "Next";
+	showQuestion();
+}
+
+
+function showQuestion() {
+	resetState();
+
+	let currentQuestion = questions[currentQuestionIndex];
+	let questionNo = currentQuestionIndex + 1;
+	questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+	currentQuestion.answers.forEach(answer => {
+		const button = document.createElement("button");
+		button.innerHTML = answer.text;
+		button.classList.add("btn");
+		answerButtons.appendChild(button);
+		
+		if(answer.correct){
+			button.dataset.correct = answer.correct;
+		}
+
+		button.addEventListener("click", selectAnswer);
+	});
+}
+
+function resetState(){
+	nextButton.style.display = "none";
+	while(answerButtons.firstChild){
+		answerButtons.removeChild(answerButtons.firstChild);
+	}
+}
+
+function selectAnswer(e){
+	const selectedBtn = e.target;
+	const isCorrect = selectedBtn.dataset.correct ==="true";
+	if(isCorrect){
+		selectedBtn.classList.add("correct");
+		score++;
+	}
+	else{
+		selectedBtn.classList.add("incorrect");
+	}
+
+	Array.from(answerButtons.children).forEach(button => {
+		if(button.dataset.correct === "true"){
+			button.classList.add("correct");
+		}
+		button.disabled = true;
+	});
+	nextButton.style.display = "block";
+}
+
+function showScore(){
+
+	resetState();
+	questionElement.innerHTML = "You Scored " + score + " out of " + questions.length + "!";
+	nextButton.innerHTML = "Play Again";
+	nextButton.style.display = "block";
+
+}
+
+function handleNextButton(){
+	currentQuestionIndex++;
+	if(currentQuestionIndex < questions.length){
+		showQuestion();
+	}else{
+		showScore();
+	}
+}
+
+nextButton.addEventListener("click", () =>{
+	if(currentQuestionIndex < questions.length){
+		handleNextButton();
+	}
+	else{
+		startQuiz();
+	}
+})
+
+startQuiz();
+
+
+
+
+
+
+
+
+
+
